f6d96c8b2dd5bddaaaccc24ee1082e6043bc8549
Marina Moskaleva <moskaleva.m.a@yandex.ru>
2021-10-31 19:44:10 +0200

    Итерация2

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,36 @@
 let content = document.querySelector('.content');
-let editButton = content.querySelector('.profile__info-editButton');
+let editButton = content.querySelector('.profile__info-edit-button');
 let popup = document.querySelector('.popup');
-let closeButton = popup.querySelector('.popup__closeButton');
+let closeButton = popup.querySelector('.popup__close-button');
 let profileName = document.querySelector('.profile__info-name-current');
 let description = document.querySelector('.profile__info-description');
 let root = document.querySelector('.root');
 
 
-let popup_name = document.querySelector('#popup_name');
-let popup_description = document.querySelector('#popup_description');
+let popupName = document.querySelector('.popup__field_content_name');
+let popupDescription = document.querySelector('.popup__field_content_description');
 
-editButton.addEventListener('click', function () {
+function openFormToEdit () {
     popup.classList.add('popup_opened');
-    popup_name.value = profileName.textContent;
-    popup_description.value = description.textContent;
-  });
+    popupName.value = profileName.textContent;
+    popupDescription.value = description.textContent;
+}
 
-closeButton.addEventListener('click', function () {
+function closeFormAfterEdit () {
     popup.classList.remove('popup_opened');
-});
+}
+
+editButton.addEventListener('click', openFormToEdit);
+
+closeButton.addEventListener('click', closeFormAfterEdit);
 
 let formElement = document.querySelector('.popup__fields');
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    profileName.textContent = popup_name.value;
-    description.textContent = popup_description.value;
+    profileName.textContent = popupName.value;
+    description.textContent = popupDescription.value;
     popup.classList.remove('popup_opened');
 }
 
 formElement.addEventListener('submit', formSubmitHandler); 
-document.addEventListener('keyup', function (event) {
-    if (event.keyCode === 13) {
-        formSubmitHandler(event);
-      }
-  });
